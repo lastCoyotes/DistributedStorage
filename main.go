47eedcf1e4a92dd566b3fe5ed79c3ff5cd9a4b56
Tristan Boler <coyotes@Tristans-MacBook-Pro.local>
2025-02-10 14:13:19 -0800

    p2p lib

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/lastcoyotes/distributedstorage/p2p"
+)
+
+func main() {
+	tr := p2p.NewTCPTransport(":3000")
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("YAP!")
+	select {}
+}
